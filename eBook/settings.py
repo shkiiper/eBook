4875c6a16e3a2b464cb2d2a4e48be377416f0675
shkiiper <shygaev.xxx@gmail.com>
2023-05-29 16:43:40 +0600

    second

--- eBook/settings.py
+++ eBook/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'user.User'  # Замените 'myapp' на имя вашего приложения, содержащего модель пользователя
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
