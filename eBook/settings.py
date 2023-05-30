c6347ef20f33b8926af443210abad1a770542c05
shkiiper <shygaev.xxx@gmail.com>
2023-05-30 10:18:34 +0600

    fix cors

--- eBook/settings.py
+++ eBook/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
 
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
