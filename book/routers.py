eab07d93238fcbbfdb581af4ceec9371d017be6c
User <user@MacBook-Pro.local>
2023-06-01 11:00:38 +0600

    Book detail

--- book/routers.py
+++ book/routers.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import BookViewSet, PageViewSet
+from .views import BookViewSet, PageViewSet, BookDetailView, PageDetailView
 
 router = routers.DefaultRouter()
 router.register(r'books', BookViewSet)
 router.register(r'pages', PageViewSet)
+router.register(r'book', BookDetailView, basename='book-detail')
+router.register(r'book/(?P<book_id>\d+)/page', PageDetailView, basename='page-detail')
+
+urlpatterns = router.urls
 
 urlpatterns = [
     path('', include(router.urls)),
--- book/views.py
+++ book/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from .models import Book, Page
@@ -15,3 +17,18 @@ class PageViewSet(viewsets.ModelViewSet):
     serializer_class = PageSerializer
     permission_classes = [AllowAny]
 
+
+class BookDetailView:
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, pk=book_id)
+        pages = Page.objects.filter(book=book).values()
+
+        return JsonResponse({'book': book.__dict__, 'pages': list(pages)})
+
+
+class PageDetailView:
+    def get(self, request, book_id, page_id):
+        book = get_object_or_404(Book, pk=book_id)
+        page = get_object_or_404(Page, pk=page_id, book=book)
+
+        return JsonResponse({'book': book.__dict__, 'page': page.__dict__})
